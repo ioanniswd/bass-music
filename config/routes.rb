5bfd0213d4916ce39dd5fcc7117b4ce90f92ac51
ioanniswd <ioanniswd@gmail.com>
2019-01-15 13:45:08 +0200

    Created album and track models. Added index and show for albums.
     On branch master
    
     Changes to be committed:
    	new file:   app/assets/javascripts/albums.coffee
    	new file:   app/assets/stylesheets/albums.css
    	new file:   app/assets/stylesheets/albums.scss
    	new file:   app/controllers/albums_controller.rb
    	new file:   app/helpers/albums_helper.rb
    	new file:   app/models/album.rb
    	new file:   app/models/track.rb
    	new file:   app/views/albums/index.html.erb
    	new file:   app/views/albums/show.html.erb
    	modified:   app/views/layouts/application.html.erb
    	modified:   config/routes.rb
    	new file:   db/migrate/20190115111335_create_albums.rb
    	new file:   db/migrate/20190115111340_create_tracks.rb
    	new file:   db/schema.rb
    	modified:   db/seeds.rb
    	new file:   test/controllers/albums_controller_test.rb
    	new file:   test/fixtures/albums.yml
    	new file:   test/fixtures/tracks.yml
    	new file:   test/models/album_test.rb
    	new file:   test/models/track_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resource :albums
+  root 'albums#index'
+  get '/albums/:id' => 'albums#show', as: :album
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
